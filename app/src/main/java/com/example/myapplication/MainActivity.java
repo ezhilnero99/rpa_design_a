7cc13dd7cd953e8a670203f083be160581d10f6c
ezhilnero99 <ezhil99mutsun@gmail.com>
2023-02-17 12:58:43 +0530

    added string

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     int variable;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
