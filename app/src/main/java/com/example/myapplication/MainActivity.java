febf9933eb15b117abefe3476fe8e9eb6841bb90
ezhilnero99 <ezhil99mutsun@gmail.com>
2023-03-03 11:43:45 +0530

    Before stashing

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,10 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-//dsksnvjnfsvnbsnvisjbnifn
-    //ewnjbwiuvbhiubiuvbfedyu
-    //skldnvoihiuvbwiauvbyy
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
